9428e379be4ea13582bc72b920bc80cb769afa99
sintu009 <topcurrentaffairs09@gmail.com>
2023-02-14 00:33:35 +0530

    project

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from '../src/pages/Home/Home.js'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Home />
   </React.StrictMode>
 );
 
